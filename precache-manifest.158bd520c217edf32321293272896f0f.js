a69414a71272f38ceee6482cf9fd9a38c024f038
tsubasa <9911tsubasa@gmail.com>
2020-08-18 17:58:35 +0900

    Updates

---
+++ precache-manifest.158bd520c217edf32321293272896f0f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab2a99d1b161525ec84535cc838ceeef",
+    "url": "/soccer/index.html"
+  },
+  {
+    "revision": "2f81cca10346683d6d91",
+    "url": "/soccer/static/css/2.0c0672e5.chunk.css"
+  },
+  {
+    "revision": "578cd6b5256fcd27b728",
+    "url": "/soccer/static/css/main.e909cbf0.chunk.css"
+  },
+  {
+    "revision": "2f81cca10346683d6d91",
+    "url": "/soccer/static/js/2.e834fe57.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/soccer/static/js/2.e834fe57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "578cd6b5256fcd27b728",
+    "url": "/soccer/static/js/main.c2dac606.chunk.js"
+  },
+  {
+    "revision": "f4d478d0a7ff9f2ba986",
+    "url": "/soccer/static/js/runtime-main.89a3cbf2.js"
+  }
+]);
